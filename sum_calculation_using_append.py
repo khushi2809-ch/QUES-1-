e9181b05d682e8275b550900ec00e7bf005dd00e
khushi2809-ch <khushuchaudhary2809@gmail.com>
2026-02-23 10:35:40 +0530

    sum_calculation_using_append.py

--- sum_calculation_using_append.py
+++ sum_calculation_using_append.py
@@ -13,4 +13,14 @@ print("Numbers =", numbers)
 print("Total Sum =", total)
 # append() → har input ko list ke end me add karta hai
 
+
 # sum(numbers) → list ke sabhi elements ka total nikalta hai
+
+#output
+# Enter number: 10
+# Enter number: 20
+# Enter number: 30
+# Enter number: 40
+# Enter number: 50
+# Numbers = [10, 20, 30, 40, 50]
+# Total Sum = 150
